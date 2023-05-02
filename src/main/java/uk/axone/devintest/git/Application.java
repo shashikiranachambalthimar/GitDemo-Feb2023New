e666642cfb9ca0783c2cbb17442628e273efbfbd
shashikirana <shashikiranadeepak@gmail.com>
2023-05-02 19:13:07 +0100

    Jira 101- Commiting project -MAY2023 Git demo

---
+++ src/main/java/uk/axone/devintest/git/Application.java
@@ -0,0 +1,11 @@
+package uk.axone.devintest.git;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Git is cool");
+    }
+
+   private void doSomething(){
+       System.out.println("Doing something");
+   }
+}
---
+++ src/test/java/uk/axone/devintest/git/tests/App_Test.java
@@ -0,0 +1,13 @@
+package uk.axone.devintest.git.tests;
+
+import org.junit.Test;
+
+public class App_Test {
+
+    @Test
+
+    public void test1(){
+        System.out.println("Running test1");
+    }
+
+}
